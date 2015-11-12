1b93e55a58d9a52a2ad47a964afba5820d2fc2c1
Víctor Barrios <sygmaz19@gmail.com>
2015-11-12 23:53:52 +0100

    A Spanish card stack

--- card_stack.cpp
+++ card_stack.cpp
@@ -12,16 +12,16 @@ using namespace std;
 
 
 struct Carta {
-	char palo;
-	int num;
-	Carta *sig; // this pointer is for iterate through the whole card stack
+    char palo;
+    int num;
+    Carta *sig; // this pointer is for iterate through the whole card stack
 };
 
 class MontonCartas
 {
-	Carta *p; // this is our stack, a simple pointer. Yeah, amazing xD
+    Carta *p; // this is our stack, a simple pointer. Yeah, amazing xD
 
-	public:
+    public:
         MontonCartas();          // constructor that builds an empty Spanish card stack
         MontonCartas(int todas); // constructor that builds a complete Spanish card stack
         ~MontonCartas();
@@ -41,69 +41,70 @@ class MontonCartas
 
 MontonCartas::MontonCartas()
 {
-	p = 0;
+    p = 0;
 }
 
 MontonCartas::MontonCartas(int todas)
 {
-	char pal [] = {'O','C','B','E'};
+    char pal [] = {'O','C','B','E'};
 
-	Carta *aux;
-	bool primeraCarta = true;
+    Carta *aux;
+    bool primeraCarta = true;
 
 
-	for(int i=0; i<4; i++)
+    for(int i=0; i<4; i++)
     {
-		char a = pal[3-i];
-		aux = new Carta;
+        char a = pal[3-i];
+        aux = new Carta;
 
-		for(int j=0; j<12; j++)
+        for(int j=0; j<12; j++)
         {
-			(*aux).palo = a;
-			(*aux).num = 12-j;
+            (*aux).palo = a;
+            (*aux).num = 12-j;
 
-			if(primeraCarta)
+            if(primeraCarta)
             {
-				(*aux).sig = 0;
-				primeraCarta = false;
-			}else
-				(*aux).sig = p;
+                (*aux).sig = 0;
+                primeraCarta = false;
+            }else
+                (*aux).sig = p;
 
-			p = aux;
+            p = aux;
 
-			aux = new Carta;
-		}
-	}
+            aux = new Carta;
+        }
+    }
 }
 
 MontonCartas::~MontonCartas()
 {
-	Carta *aux;
+    Carta *aux;
 
-	aux = (*p).sig;
-	while(aux != 0){
-		delete p;
-		p = aux;
-		aux = (*aux).sig;
-	}
-	delete p;
+    aux = (*p).sig;
+    while(aux != 0)
+    {
+        delete p;
+        p = aux;
+        aux = (*aux).sig;
+    }
+    delete p;
 }
 
 MontonCartas& MontonCartas::operator= (const MontonCartas& orig)
 {
-	if(&orig != this)
+    if(&orig != this)
     {
-		if(this->p != 0)
+        if(this->p != 0)
         {
-			Carta *Claus;
-			Claus=this->p;
-			while(Claus != 0)
-			{
-				Claus = (*Claus).sig;
-				delete p;
-				p = Claus;
-			}
-		}
+            Carta *Claus;
+            Claus=this->p;
+            while(Claus != 0)
+            {
+                Claus = (*Claus).sig;
+                delete p;
+                p = Claus;
+            }
+        }
 
         Carta *aux, *aux2;
         aux = this->p;
@@ -114,7 +115,8 @@ MontonCartas& MontonCartas::operator= (const MontonCartas& orig)
         {
             aux = new Carta;
 
-            if(primeraCarta){
+            if(primeraCarta)
+            {
                 (*aux).sig = 0;
                 primeraCarta = false;
             }else
@@ -136,137 +138,141 @@ MontonCartas& MontonCartas::operator= (const MontonCartas& orig)
             aux = (*aux).sig;
             aux2 = (*aux2).sig;
         }
-	}
-	return *this;
+    }
+    return *this;
 }
 
 void MontonCartas::mezclar()
 {
-	Carta *aux;
-	aux = p;
-	int tam=0;
-	for(tam; aux != 0; tam++)
-		aux = (*aux).sig;
+    Carta *aux;
+    aux = p;
+    int tam=0;
+    for(tam; aux != 0; tam++)
+    aux = (*aux).sig;
 
-	Carta *aux2;
+    Carta *aux2;
 
-	for(int i=0; i<tam; i++)
+    for(int i=0; i<tam; i++)
     {
-		aux = p;
-		aux2 = p;
+        aux = p;
+        aux2 = p;
 
-		srand(time(0));
-		int a = rand()%tam;
-		int b = rand()%tam;
+        srand(time(0));
+        int a = rand()%tam;
+        int b = rand()%tam;
 
-		Carta *aux2a;
-		Carta *auxan;
-		aux2a = p;
-		auxan = p;
+        Carta *aux2a;
+        Carta *auxan;
+        aux2a = p;
+        auxan = p;
 
-		for(int h=0; h<a; h++)
-			aux = (*aux).sig;
-		for(int h=0; h<a-1; h++)
-			auxan = (*auxan).sig;
-		for(int h=0; h<b; h++)
-			aux2 = (*aux2).sig;
-		for(int h=0; h<b-1; h++)
-			aux2a = (*aux2a).sig;
+        for(int h=0; h<a; h++)
+            aux = (*aux).sig;
+        for(int h=0; h<a-1; h++)
+            auxan = (*auxan).sig;
+        for(int h=0; h<b; h++)
+            aux2 = (*aux2).sig;
+        for(int h=0; h<b-1; h++)
+            aux2a = (*aux2a).sig;
 
-		if( (&aux != &aux2) && (&aux != &auxan) && (&aux2 != &aux2a) )
+        if( (&aux != &aux2) && (&aux != &auxan) && (&aux2 != &aux2a) )
         {
-			(*aux).sig = (*aux2).sig;
-			(*aux2).sig = aux2a;
-			(*aux2a).sig = aux;
-			(*auxan).sig = aux2;
-		}
-		else if( (&aux == &auxan) || (&aux2 == &aux2a) )
+            (*aux).sig = (*aux2).sig;
+            (*aux2).sig = aux2a;
+            (*aux2a).sig = aux;
+            (*auxan).sig = aux2;
+        }
+        else if( (&aux == &auxan) || (&aux2 == &aux2a) )
         {//que sera cuando estén al principio
 
-			(*aux).sig = (*aux2).sig;
-			(*aux2).sig = aux2a;
-			(*aux2a).sig = aux;
-			p = aux2;
-		}
-	}
+            (*aux).sig = (*aux2).sig;
+            (*aux2).sig = aux2a;
+            (*aux2a).sig = aux;
+            p = aux2;
+        }
+    }
 }
 
 void MontonCartas::eliminarCarta (int pos)
 {
-	Carta *aux;
-	Carta *aux2;
-	aux = p;
-	aux2 = p;
-	for(int i=0; i<pos; i++)
-		aux = (*aux).sig;
-	for(int i=0; i<pos-1; i++)
-		aux2 = (*aux2).sig;
+    Carta *aux;
+    Carta *aux2;
+    aux = p;
+    aux2 = p;
 
-	(*aux2).sig = (*aux).sig;
+    for(int i=0; i<pos; i++)
+        aux = (*aux).sig;
+    for(int i=0; i<pos-1; i++)
+        aux2 = (*aux2).sig;
 
-	delete aux;
+    (*aux2).sig = (*aux).sig;
+
+    delete aux;
 }
 
 int MontonCartas::getNumeroCarta (int pos) const
 {
-	Carta *aux = p;
-	for(int i=0; i<pos; i++)
-		aux = (*aux).sig;
+    Carta *aux = p;
+    for(int i=0; i<pos; i++)
+        aux = (*aux).sig;
 
-	return (*aux).num;
+    return (*aux).num;
 }
 
 char MontonCartas::getPaloCarta (int pos) const
 {
-	Carta *aux;
-	aux = p;
-	for(int i=0; i<pos; i++)
-		aux = (*aux).sig;
+    Carta *aux;
+    aux = p;
+    for(int i=0; i<pos; i++)
+        aux = (*aux).sig;
 
-	return (*aux).palo;
+    return (*aux).palo;
 }
 
 void MontonCartas::insertarCarta (int n, char tipo)
 {
-	//1º buscamos alguna carta igual en numero y palo
-	bool encontrada = false;
-	Carta *aux;
-	aux = p;
-	while(aux!=0 && !encontrada)
+    //1º buscamos alguna carta igual en numero y palo
+    bool encontrada = false;
+    Carta *aux;
+    aux = p;
+
+    while(aux!=0 && !encontrada)
     {
-		if((*aux).num == n && (*aux).palo == tipo)
-			encontrada = true;
-		aux = (*aux).sig;
-	}
+        if((*aux).num == n && (*aux).palo == tipo)
+        encontrada = true;
+        aux = (*aux).sig;
+    }
 
-	//ahora la añadimos al final
-	if(!encontrada){
-		aux = p;
-		//movemos aux hasta el ultimo lugar
-		while((*aux).sig != 0){
-			aux = (*aux).sig;
-		}
+    //ahora la añadimos al final
+    if(!encontrada)
+    {
+        aux = p;
+        //movemos aux hasta el ultimo lugar
+        while((*aux).sig != 0)
+        {
+            aux = (*aux).sig;
+        }
 
-		//creamos la nueva carta
-		Carta *aux2 = new Carta;
-		(*aux2).sig = 0;
-		(*aux2).num = n;
-		(*aux2).palo = tipo;
+        //creamos la nueva carta
+        Carta *aux2 = new Carta;
+        (*aux2).sig = 0;
+        (*aux2).num = n;
+        (*aux2).palo = tipo;
 
-		(*aux).sig = aux2;
-	}else
-		cout<<"No se pudo añadir porque ya existe."<<endl;
+        (*aux).sig = aux2;
+    }else
+        cout<<"No se pudo añadir porque ya existe."<<endl;
 }
 
 int MontonCartas::sizeMonton() const
 {
-	int tam=0;
-	Carta *aux;
-	aux = p;
-	for(tam; aux != 0; tam++)
-		aux = (*aux).sig;
+    int tam=0;
+    Carta *aux;
+    aux = p;
+    for(tam; aux != 0; tam++)
+        aux = (*aux).sig;
 
-	return tam;
+    return tam;
 }
 
 void MontonCartas::printMonton()
@@ -279,7 +285,8 @@ void MontonCartas::printMonton()
         if(palo == '#')
             palo = (*aux).palo ;
 
-        if(palo != (*aux).palo) {
+        if(palo != (*aux).palo)
+        {
             palo = (*aux).palo ;
             cout << endl;
         }
@@ -293,7 +300,7 @@ void MontonCartas::printMonton()
 
 int main()
 {
-	MontonCartas m(0);
+    MontonCartas m(0);
 
     m.printMonton();
 }
