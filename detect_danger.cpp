fc48adf81be2eaa46c52fb791e4ef4b8f33cf52e
Víctor Barrios <sygmaz19@gmail.com>
2015-11-12 18:47:03 +0100

    Updated code

--- detect_danger.cpp
+++ detect_danger.cpp
@@ -5,7 +5,7 @@ int main()
 {
     // x0 y0 z0 are coordinates in the origin
 	double radius, x0, y0, z0=0;
-	cout<<"Please introduce geometric data to build our scanner (radius x y):"<<endl;
+	cout<<"Please introduce geometric data to build our radar (radius x y):"<<endl;
 	cin>> radius >> x0 >> y0;
 	cout<<endl;
 
@@ -20,41 +20,54 @@ int main()
     // perimA : perimeter of action of the air target
 	double xt, yt, xa, ya, za, perimT, perimA;
 
-	// Says if the target is dangerous or not.
-	// If the target is inside our area, of course it is. On the other hand, no.
-	bool amenaza=false;
 
-	// This var
+	// This var indicates if the target is a land target or an air target.
+	// Specified by the user.
 	char letra;
 
-	do{
-        cout << "What type of target is it? (T or A)"
-		cin>>letra;
-		if(letra=='T' && !amenaza){
+	do {
+        cout << "What type of target is it, T (land), A (air) or D (none)?: " ;
+		cin >> letra;
+		cout << endl;
 
-			cin>>xt>>yt;
-			perimT=pow(xt-x0,2)+pow(yt-y0,2);
+		if(letra=='T')
+        {
+            cout << "Land target detected" << endl;
+            cout << "Please give me its XY coordinates: ";
+			cin >> xt >> yt;
 
-			if(perimT<r){
-					cout<<"AMENAZA TERRESTRE."<<endl;
-					amenaza=true;
-			}
+			perimT = pow(xt-x0,2)+pow(yt-y0,2); // calculate the action perimeter
+
+			if(perimT < radius)
+            {
+                cout<<"ALERT! LAND THREAT!"<<endl;
+			}else
+                cout << "It's OK" <<endl;
 		}
 
-		if(letra=='A' && !amenaza){
+		if(letra=='A')
+        {
+            cout << "Air target detected" << endl;
+            cout << "Please give me its XYZ coordinates: ";
 
-			cin>>xa>>ya>>za;
+			cin >> xa >> ya >> za;
+
 			perimA=pow(xa-x0,2)+pow(ya-y0,2)+za*za;
 
-			if(perimA<r){
-				cout<<"AMENAZA AEREA."<<endl;
-				amenaza=true;
-			}
+			if(perimA < radius)
+            {
+				cout<<"ALERT! AIR THREAT!"<<endl;
+			}else
+                cout << "It's OK" <<endl;
 		}
 
-		if(letra=='D' && !amenaza){
-			cout<<"NINGUNA AMENAZA."<<endl;
-			amenaza=true;
+		if(letra=='D')
+        {
+			cout<<"None Threat"<<endl;
+			cout << "Good Bye" << endl;
 		}
-	}while(letra!='D'||amenaza!=true);
+
+		cout << endl;
+
+	} while( letra != 'D' );
 }
